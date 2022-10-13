67f895b5b4402d4dd4d0b69b23f6607ce4578536
Josh Scott <60557820+joshuabscott@users.noreply.github.com>
2022-10-12 22:18:16 -0700

    ver1.9
    QR code Two Factor Auth Setup

--- Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -181,7 +181,7 @@ namespace NeoContact.Areas.Identity.Pages.Account.Manage
             return string.Format(
                 CultureInfo.InvariantCulture,
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("Microsoft.AspNetCore.Identity.UI"),
+                _urlEncoder.Encode("Neo Contact"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
